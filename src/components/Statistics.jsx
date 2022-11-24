686428813189cd9dc283ad024e8c8e7437ffb0f8
Marta <maardiakiv@gmail.com>
2022-11-24 19:57:21 +0100

    edited PropTypes import

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,4 +1,4 @@
-import PropTypes, { element } from 'prop-types';
+import PropTypes from 'prop-types';
 // import css from './Statistics.module.css';
 
 export const Statistics = ({ title, stats }) => {
--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -1,4 +1,4 @@
-import PropTypes, { element } from 'prop-types';
+import PropTypes from 'prop-types';
 // import css from './FriendList.module.css';
 
 export const TransactionHistory = ({ items }) => {
