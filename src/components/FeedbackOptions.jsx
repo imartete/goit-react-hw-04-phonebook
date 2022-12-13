fb0df782a0f856403f5adf5f4aaa8a9def24f50d
Marta <maardiakiv@gmail.com>
2022-12-13 15:05:29 +0100

    make buttons by iterating

--- src/components/FeedbackOptions.jsx
+++ src/components/FeedbackOptions.jsx
@@ -3,19 +3,20 @@ import PropTypes from 'prop-types';
 export const FeedbackOptions = ({ options, onLeaveFeeback }) => {
   return (
     <div>
-      <button type="button" id="good" onClick={onLeaveFeeback}>
-        Good
-      </button>
-      <button type="button" id="neutral" onClick={onLeaveFeeback}>
-        Neutral
-      </button>
-      <button type="button" id="bad" onClick={onLeaveFeeback}>
-        Bad
-      </button>
+      {Object.keys(options).map(key => (
+        <button type="button" key={key} id={key} onClick={onLeaveFeeback}>
+          {key[0].toUpperCase() + key.slice(1)}
+        </button>
+      ))}
     </div>
   );
 };
 
 FeedbackOptions.propTypes = {
+  options: PropTypes.shape({
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+  }).isRequired,
   onLeaveFeeback: PropTypes.func.isRequired,
 };
