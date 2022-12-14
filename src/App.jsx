64e1a45cb5db9124639173836ac28e1c4f2db773
Marta <maardiakiv@gmail.com>
2022-12-14 20:32:26 +0100

    remove section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Section } from 'components/Section';
 import { ContactForm } from 'components/ContactForm';
 import { ContactList } from 'components/ContactList';
 import { Filter } from 'components/SearcFilter';
