d4f960f9dcd986701f83c8a6d4cb48955a8d855b
Marta <maardiakiv@gmail.com>
2022-12-29 20:02:11 +0100

    reset state on form submit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -16,7 +16,8 @@ export function ContactForm({ onSubmit }) {
   function handleSubmit(event) {
     event.preventDefault();
     onSubmit({ name, number });
-    event.currentTarget.reset();
+    setName('');
+    setNumber('');
   }
 
   return (
@@ -26,6 +27,7 @@ export function ContactForm({ onSubmit }) {
         <input
           type="text"
           name="name"
+          value={name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
@@ -37,6 +39,7 @@ export function ContactForm({ onSubmit }) {
         <input
           type="tel"
           name="number"
+          value={number}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
